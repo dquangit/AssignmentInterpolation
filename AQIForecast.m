89a858b777df0b32790a2ec397ac0176fd2a60f6
Quang Dep Trai <phamvanquangit@gmail.com>
2018-06-23 01:56:50 +0700

    Calculate aqi

--- AQIForecast.m
+++ AQIForecast.m
@@ -203,19 +203,55 @@ end
 
 % --- Executes on button press in searchSelectDayButton.
 function searchSelectDayButton_Callback(hObject, eventdata, handles)
-% hObject    handle to searchSelectDayButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-uicalendar('Weekend', [1 0 0 0 0 0 1], ...  
-'SelectionType', 1, ...  
-'DestinationUI',handles.searchDay);
+    % hObject    handle to searchSelectDayButton (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+    uicalendar('Weekend', [1 0 0 0 0 0 1], ...  
+    'SelectionType', 1, ...  
+    'DestinationUI', {handles.searchDay, 'String'});
+    waitfor({handles.searchDay, 'String'});
+    selectedDate = get(handles.searchDay, 'String');
+    disp('Fuck');
 
 
 % --- Executes on button press in calExecute.
 function calExecute_Callback(hObject, eventdata, handles)
-% hObject    handle to calExecute (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+    tempInput = get(handles.calTemp,'String');
+    rainInput = get(handles.calRain,'String'); 
+    [~, tempData, rainData, aqiData] = loadRealData();
+    aqiLength = length(aqiData);
+    tempLength = length(tempData);
+    rainLength = length(rainData);
+    minimum = min([aqiLength tempLength rainLength]);
+    %disp([aqiLength tempLength rainLength]);
+    aqiData = aqiData(1 : minimum);
+    tempData = tempData(1 : minimum);
+    rainData = rainData(1 : minimum);
+    result = calculateAqi(tempData, rainData, aqiData, tempInput, rainInput);
+    if result == -1 
+        msgbox('Invalidate input');
+    else
+        set(handles.calResult, 'string', num2str(result));
+        if result <= 50
+            set(handles.calEvaluate,'String','Good');
+            set(handles.calWarning,'String','Air quality is considered satisfactory, and air pollution poses little or no risk');
+        elseif result <= 100
+            set(handles.calEvaluate,'String','Moderate');
+            set(handles.calWarning,'String','Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.');
+        elseif result <= 150
+            set(handles.calEvaluate,'String','Unhealthy for Sensitive Groups');
+            set(handles.calWarning,'String','Members of sensitive groups may experience health effects. The general public is not likely to be affected.');
+        elseif result <= 200
+            set(handles.calEvaluate,'String','Unhealthy');
+            set(handles.calWarning,'String', 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects');
+        elseif result <= 300
+            set(handles.evaluate,'String','Very Unhealthy');
+            set(handles.calWarning, 'String', 'Health warnings of emergency conditions. The entire population is more likely to be affected.');
+        else
+            set(handles.calEvaluate,'String','Good');
+            set(handles.calWarning, 'String', 'Health alert: everyone may experience more serious health effects.');
+        end
+    end
 
 
 % --- Executes on selection change in listbox1.
