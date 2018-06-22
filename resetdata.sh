e8e3d83fbfde3137e417a748167e7ac344750349
Quang Dep Trai <phamvanquangit@gmail.com>
2018-06-23 00:08:23 +0700

    reset data

--- resetdata.sh
+++ resetdata.sh
@@ -1,6 +1,12 @@
-git checkout date.txt
-git checkout aqi03-072017.txt
-git checkout rain03-052017.txt
-git checkout temp03-052017.txt
+git checkout data/realdata/date.txt
+git checkout data/realdata/aqi.txt
+git checkout data/realdata/rain.txt
+git checkout data/realdata/temp.txt
+
+git checkout data/forecastdata/date.txt
+git checkout data/forecastdata/aqi.txt
+git checkout data/forecastdata/rain.txt
+git checkout data/forecastdata/temp.txt
+
 
 echo 'OK'
