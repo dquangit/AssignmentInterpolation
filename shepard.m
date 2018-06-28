207afeeeda04a61ab11c007117d754cc34b667c8
DQuang <phamvanquangit@gmail.com>
2018-06-28 15:42:08 +0700

    Plot shepard

---
+++ shepard.m
@@ -0,0 +1,28 @@
+function out = shepard(X, Y, x, k)
+    sumH = 0;
+    lengthInput = length(X);
+    
+    for index = 1 : lengthInput
+        h(index) = abs(x - X(index))^-k;
+        sumH = sumH + h(index);
+    end
+
+    w = zeros(size(X));
+    for index = 1 : lengthInput
+        w(index) = h(index)/sumH;
+    end
+
+    out  = 0;
+    for index = 1 : lengthInput
+       out = out + (w(index)*Y(index));
+    end
+    disp(out);
+    if isnan(out)
+        for index = lengthInput : -1 : 1
+            if (X(index) == x)
+                out = Y(index);
+                break;
+            end
+        end
+    end
+end
